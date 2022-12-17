d3114b2c7722ab5624dbaf8f682f9c46a9e91e95
PlayerThird030 <verevka.rud@gmail.com>
2022-12-17 22:47:53 +0300

    Example 10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {12,32,31,44,55,456,767,878};
+
+int n = array.Length;
+int find = 878;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
